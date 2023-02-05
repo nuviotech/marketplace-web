9b27e33dce2f3c5d19dc0f3bd1ad9fc3ace2d923
jayhindPrajapati07 <114147639+jayhindPrajapati07@users.noreply.github.com>
2023-02-05 21:03:51 +0530

    updates repository.js

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,11 +1,12 @@
 import axios from 'axios';
-const baseDomain = 'http://localhost:1337'; // API for products
-export const basePostUrl = 'http://localhost:1337'; // API for post
-export const baseStoreURL = 'http://localhost:1337'; // API for vendor(store)
+const baseDomain = 'http://manageecom.com:1337'; // API for products
+export const basePostUrl = 'http://manageecom.com:1337'; // API for post
+export const baseStoreURL = 'http://manageecom.com:1337'; // API for vendor(store)
 
-export const sellerProductUrl = 'http://localhost:9090'; // API seller product url
-export const sellerPolicyUrl = "http://localhost:9001";//change the name policy maker  url
-export const marketplaceUrl = "http://localhost:8089";//marketplace url
+
+export const sellerProductUrl = 'http://manageecom.com:8081'; // API seller product url
+export const sellerPolicyUrl = "http://manageecom.com:8083";//change the name policy maker  url
+export const marketplaceUrl = "http://manageecom.com:8084";//marketplace api url
 
 export const customHeaders = {
     Accept: 'application/json',
