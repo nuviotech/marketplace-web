09b829e36dec5057aa988d8e61ea06ff8127a10e
prasuna.pannati <prasuna.pannati@gmail.com>
2023-03-05 07:58:28 +0000

    05-03-2023

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 const baseDomain = 'https://nuvio.in:8443'; // API for products
 export const basePostUrl = 'https://nuvio.in:8443'; // API for post
 export const baseStoreURL = 'https://nuvio.in:8443'; // API for vendor(store)
-/*
+
 export const sellerProductUrl = 'https://manageecom.com:8001'; // API seller product url
-export const sellerPolicyUrl = "https://manageecom.com:8003";//change the name policy maker  url
-export const marketplaceUrl = "https://manageecom.com:8004";//marketplace api url
-*/
+export const sellerPolicyUrl = "https://nuviosellers.com:8003/";//change the name policy maker  url
+export const marketplaceUrl = "https://nuvio.in:8004";//marketplace api url
+/*
 export const sellerProductUrl = 'http://localhost:8083'; // API seller product url
 export const sellerPolicyUrl = "http://localhost:9001";//change the name policy maker  url
 export const marketplaceUrl = "http://localhost:8089";//marketplace api url
-
+*/
 export const customHeaders = {
     Accept: 'application/json',
 	//'Access-Control-Allow-Origin': 'https://manageecom.com:8001,https://manageecom.com:8003,https://manageecom.com:8004,https://nuvio.in:8443,https://nuvio.in'
