bdb4dd45ea0a0fd09984375a06c01aec2efadbd5
rk0775 <118426413+rk0775@users.noreply.github.com>
2023-05-01 10:26:21 +0530

    01-05-2023

--- components/elements/Rating.jsx
+++ components/elements/Rating.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 const Rating = () => (
-    <span className="ps-rating">
+   {/* <span className="ps-rating">
         <i className="fa fa-star"></i>
         <i className="fa fa-star"></i>
         <i className="fa fa-star"></i>
         <i className="fa fa-star"></i>
         <i className="fa fa-star-o"></i>
     </span>
+    */}
 );
 
 export default Rating;
