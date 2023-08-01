1783ba4a2eee50372d7b20466543caf68b3896ab
PrasunaPannati <112516442+PrasunaPannati@users.noreply.github.com>
2023-08-01 21:50:48 +0530

    Update OrderRoute.js

--- pages/api/OrderRoute.js
+++ pages/api/OrderRoute.js
@@ -5,7 +5,7 @@ import { getToken } from "~/store/auth/action";
 
 export default function handler(req,res){
     const reqMethod = req.method;
-    if(reqMethod==="POST"){
+    if(reqMethod==='POST'){
         const flag = req.query.flag;
         const transactionId = req.query.txid;
        // return res.redirect(307,`http://localhost:3000/account/orders?flag=${flag}&txid=${transactionId}`);
