fe6b12189f052a32161225b8954287b080266232
PrasunaPannati <112516442+PrasunaPannati@users.noreply.github.com>
2023-08-01 21:49:08 +0530

    Update OrderRoute.js

--- pages/api/OrderRoute.js
+++ pages/api/OrderRoute.js
@@ -1,20 +1,17 @@
-/*import Axios from "axios";
+import Axios from "axios";
 import { NextResponse } from "next/server";
 import { marketplaceUrl } from "~/repositories/Repository";
 import { getToken } from "~/store/auth/action";
 
 export default function handler(req,res){
     const reqMethod = req.method;
-    if(reqMethod=="POST"){
+    if(reqMethod==="POST"){
         const flag = req.query.flag;
         const transactionId = req.query.txid;
        // return res.redirect(307,`http://localhost:3000/account/orders?flag=${flag}&txid=${transactionId}`);
       // return res.redirect(307,`https://nuvio.in/account/orders?flag=${flag}&txid=${transactionId}`);
-       return res.writeHead(302, {
-        Location: `https://nuvio.in/account/orders?flag=${flag}&txid=${transactionId}`,
-        //add other headers here...
-      })
+       res.status(200).json({ message: 'This is a POST request' });
     
     }
     
-}*/
+}
