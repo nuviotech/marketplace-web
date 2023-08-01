a96d73bff82ad5abfd4462f3662c8fd9c7dfea3f
PrasunaPannati <112516442+PrasunaPannati@users.noreply.github.com>
2023-08-01 22:11:09 +0530

    Update OrderRoute.js

--- pages/api/OrderRoute.js
+++ pages/api/OrderRoute.js
@@ -11,7 +11,7 @@ export default function handler(req,res){
        // return res.redirect(307,`http://localhost:3000/account/orders?flag=${flag}&txid=${transactionId}`);
       // return res.redirect(307,`https://nuvio.in/account/orders?flag=${flag}&txid=${transactionId}`);
        res.status(200).json({ message: 'This is a POST request' });
-    
+        res.writeHead(302, { Location: '/account/orders' });
     }
     
 }
